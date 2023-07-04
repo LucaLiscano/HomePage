2638bceba6d39a34d515a11c68762ee734256b9b
unknown <luca.liscano@anima.edu.uy>
2023-07-04 11:55:10 -0300

    Adelanto del header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import './Header.css'
+
+
+const Header = () => {
+  return (
+	<>
+	<header id="Header">
+		<a href="">
+		<img src="https://www.pngmart.com/files/17/Cartoon-Rocket-Raccoon-PNG-Clipart.png" alt="" />
+		</a>
+		<nav>
+			<ul>
+				<li><a href="">Sobre Nosotros</a></li>
+				<li><a href="">Tienda</a></li>
+				<li><a href="">Donaciones</a></li>
+			</ul>
+		</nav>
+	</header>
+	</>
+  )
+}
+
+export default Header
